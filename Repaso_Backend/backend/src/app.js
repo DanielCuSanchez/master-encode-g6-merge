360de298305cfde89fdafd436778244309f2a124
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-10-25 20:36:20 -0500

    Fix bug eslint

--- Repaso_Backend/backend/src/app.js
+++ Repaso_Backend/backend/src/app.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const { router } = require("./routes")
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const { router } = require('./routes')
 const app = express()
-//Server port
+// Server port
 const PORT = process.env.PORT || 4000
-//Middlewares
+// Middlewares
 app.use(express.json())
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(cors())
-//Routes
+// Routes
 app.use(router)
 
 module.exports = { app, PORT }
--- Repaso_Backend/backend/src/routes/index.js
+++ Repaso_Backend/backend/src/routes/index.js
@@ -1,18 +1,17 @@
-const express = require("express")
+const express = require('express')
 
 const router = express.Router()
 
-//Autorización
-router.post("/login", (req, res) => {
-  res.status(200).json({ msg: "Login" })
+// Autorización
+router.post('/login', (req, res) => {
+  res.status(200).json({ msg: 'Login' })
 })
 // Usuarios
-router.get("/usuarios", (req, res) => {
-  res.status(200).json({ msg: "usuarios" })
+router.get('/usuarios', (req, res) => {
+  res.status(200).json({ msg: 'usuarios' })
 })
-router.put("/usuarios/:id", (req, res) => {
-  res.status(200).json({ msg: "usuarios/:id" })
+router.put('/usuarios/:id', (req, res) => {
+  res.status(200).json({ msg: 'usuarios/:id' })
 })
-
 
 module.exports = { router }
