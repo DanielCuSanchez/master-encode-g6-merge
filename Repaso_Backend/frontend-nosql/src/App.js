a14c1b1d1d0e9b7b80f1d741dcde18300f6fdff8
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2021-11-08 21:32:05 -0600

    Frontend nosql

---
+++ Repaso_Backend/frontend-nosql/src/App.js
@@ -0,0 +1,13 @@
+import { FormUser } from "./Componentes/FormUser";
+
+function App() {
+  return (
+    <div >
+      <p className="uppercase text-white mx-4 my-4">hola mundo</p>
+
+      <FormUser />
+    </div>
+  );
+}
+
+export default App;
---
+++ Repaso_Backend/frontend-nosql/tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
