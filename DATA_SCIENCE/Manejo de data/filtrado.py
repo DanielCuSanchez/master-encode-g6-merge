99f9d8bfd9afbfba97abe79bba0f48f6861b8f9f
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-02-09 19:51:12 -0600

    Registros nuevos

---
+++ DATA_SCIENCE/Manejo de data/filtrado.py
@@ -0,0 +1,93 @@
+DATA = [
+    {
+        'name': 'Andrea',
+        'age': 72,
+        'organization': 'DEVF',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'DEVF',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'DEVF',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'DEVF',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+    {
+        'name': 'Genaro',
+        'age': 40,
+        'organization': 'JS Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+    {
+        'name': 'Pedro',
+        'age': 23,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    # Filter
+    ejemplo=1
+
+if __name__ == '__main__':
+    run()
